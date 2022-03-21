5764d6417f298e13790f6a5bc900438bd7138412
Yunosuke Minegishi <y.minegishi0924@gmail.com>
2022-03-22 01:43:17 +0900

    add Reactの環境構築

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,19 @@ const path = require('path')
 
 module.exports = {
   // 最初に読み込むファイルを指定
-  entry: './src/index.js',
-  entry: './src/index.ts',
+  entry: './src/index.tsx',
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.tsx$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
+  // モジュールとして解決する、つまり外部ファイルやライブラリ（node_modules 以下のファイル）を使うファイルの拡張子
   resolve: {
-    extensions: ['.ts'],
+    extensions: ['.js', '.ts', '.tsx'],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
